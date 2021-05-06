b65d7a4332af68b36dada4224ce4abc043ed90d8
ANKIT YADAV <ankity1707@outlook.com>
2021-05-06 23:49:47 +0530

    Add project files.

---
+++ GoogleAdmob-Xamarin/GoogleAdmob-Xamarin/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using GoogleAdmob;
+using GoogleAdmob.Utilities;
+using MarcTron.Plugin;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GoogleAdmob_Xamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            bannerAd.AdsId = StringReference.BannerId;
+            CrossMTAdmob.Current.OnInterstitialLoaded += (sender, e) => { CrossMTAdmob.Current.ShowInterstitial(); };
+            CrossMTAdmob.Current.OnRewardedVideoAdLoaded += (sender, e) => { CrossMTAdmob.Current.ShowRewardedVideo(); };
+        }
+
+        private void interstitial_Clicked(object sender, EventArgs e)
+        {
+            CrossMTAdmob.Current.LoadInterstitial(StringReference.InterstitialId);
+        }
+
+        private void reward_Clicked(object sender, EventArgs e)
+        {
+            CrossMTAdmob.Current.LoadRewardedVideo(StringReference.VideoId);
+        }
+
+        
+    }
+}
